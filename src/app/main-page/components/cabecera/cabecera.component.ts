a7c2f66e5f55698f701877b34b177035832a009f
daniel04lope <daniel04lope@gmail.com>
2025-04-21 16:39:33 +0200

    Cambios en interfaz y assets a;adidos para la ventana principal

--- src/app/main-page/components/cabecera/cabecera.component.ts
+++ src/app/main-page/components/cabecera/cabecera.component.ts
@@ -1,11 +1,16 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router'; 
+import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-cabecera',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule, ], // ⬅️ Asegúrate de importar RouterModule
   templateUrl: './cabecera.component.html',
   styleUrls: ['./cabecera.component.css']
 })
@@ -14,8 +19,14 @@ export class CabeceraComponent {
   
   @Output() buscar = new EventEmitter<string>();
 
+  constructor(private router: Router) {}
+
   onBuscar() {
     this.buscar.emit(this.terminoBusqueda);
   }
-  
+
+  // Función para saber si estamos en la ruta raíz
+  get mostrarBarraBusqueda(): boolean {
+    return this.router.url === '/';
+  }
 }
