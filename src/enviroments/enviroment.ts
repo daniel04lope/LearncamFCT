4482e4c7836cc38fed9333fc34f83d2aa2401150
daniel04lope <157105562+daniel04lope@users.noreply.github.com>
2025-04-30 13:50:54 +0200

    Subida de archivos

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ3js_JvdHSdcpgQ27rvTjL9DfUHvs3c0",
+  authDomain: "learncamfct.firebaseapp.com",
+  projectId: "learncamfct",
+  storageBucket: "learncamfct.firebasestorage.app",
+  messagingSenderId: "742667960354",
+  appId: "1:742667960354:web:06e2a14e2e2e8d0e9cca7b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
